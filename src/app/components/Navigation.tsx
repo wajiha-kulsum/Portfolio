a070f727374e1df31c2444749a8e8efabc12caaa
Wajiha Kulsum <140605975+wajiha-kulsum@users.noreply.github.com>
2025-07-09 00:44:13 +0530

    Update Navigation.tsx

--- src/app/components/Navigation.tsx
+++ src/app/components/Navigation.tsx
@@ -12,7 +12,6 @@ const Navigation = () => {
   const navItems = [
     { id: 'home', label: 'Home', href: '#' },
     { id: 'about', label: 'About', href: '#about-section' }, 
-     { id: 'about', label: 'skills', href: '#about-section' }, 
     { id: 'projects', label: 'Projects', href: '#projects-section' },
     { id: 'contact', label: 'Contact', href: '#contact-section' },
   ];
